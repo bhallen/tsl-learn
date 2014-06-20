473a11ae4c2a4fe1690e0aa7cd13d1cd5bf189c3
bhallen <allenblakeh@gmail.com>
2014-06-19 17:22:45 -0700

    learn.py works!

--- learn.py
+++ learn.py
@@ -7,18 +7,34 @@ class Grammar(dict):
     """
     """
     def __init__(self, training, sigma=None, max_k=2):
+        """
+        Attributes:
+        lexicon
+        sigma
+        max_k
+        tiers
+        grammatical
+        """
         self.read_training_file(training)
         self.get_sigma(sigma)
         self.max_k = max_k
+        self.build_tiers()
 
-        self.tiers = []
 
-
-    def learn():
+    def learn(self):
         """
         """
-        # 
-        pass
+        self.grammatical = {}
+        for tier in self.tiers:
+            tier_grammatical = []
+            for form in self.lexicon:
+                on_tier = [s for s in form if s in tier]
+                # print(tier)
+                # print(form)
+                # print(on_tier)
+                # print() 
+                tier_grammatical += self.get_ngrams(on_tier, self.max_k)
+            self.grammatical[tuple(tier)] = set(tier_grammatical)
 
 
     def read_training_file(self, training_file):
@@ -33,15 +49,30 @@ class Grammar(dict):
             merged_data = list(itertools.chain.from_iterable(self.lexicon))
             self.sigma = list(set(merged_data))
         else:
-            # read file
+            # TO-DO: read file
             pass
 
-    def ngrams(self, sequence, max_k):
-        return list(itertools.chain.from_iterable([zip(*[sequence[i:] for i in range(n)]) for n in max_k]))
+
+    def build_tiers(self):
+        """All possible tiers
+        """
+        self.tiers = list(itertools.chain.from_iterable(itertools.combinations(self.sigma, r) for r in range(1,len(self.sigma)+1)))
+
+
+    def get_ngrams(self, sequence, max_k):
+        return list(itertools.chain.from_iterable([zip(*[sequence[i:] for i in range(n)]) for n in range(max_k+1)]))
 
 
 if __name__ == '__main__':
     # TESTING
     g = Grammar('training_unbounded_harm_restricted.txt')
-    print(g.lexicon)
-    print(g.sigma)
+    # print(g.lexicon)
+    # print(g.sigma)
+    # print(g.tiers)
+
+    g.learn()
+    # print(g.grammatical)
+    for t in g.grammatical:
+        print(t)
+        print(g.grammatical[t])
+        print()
